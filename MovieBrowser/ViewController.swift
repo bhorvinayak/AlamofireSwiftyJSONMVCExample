25d61377cf46a53a59f524f7c3185589a7c706e9
Vinayak <vinayak@Vinayaks-Mac.local>
2018-09-23 03:05:20 -0700

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Vinayak on 23/09/18.
+//  Copyright © 2018 Vinayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
